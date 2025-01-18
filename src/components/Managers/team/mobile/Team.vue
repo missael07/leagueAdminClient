9a721fcca2abe1ac1e003814facfb7a0f7d73db2
mpadillaReliant <missael@reliantparking.com>
2025-01-17 16:25:09 -0800

    fix team design

--- src/components/Managers/team/mobile/Team.vue
+++ src/components/Managers/team/mobile/Team.vue
@@ -1,25 +1,25 @@
 <template>
     <v-container grid-list-xs>
-        <v-card outlined class="pa-3">
+        <v-card outlined class="pa-2">
             <!-- Header -->
-            <v-card-title class="d-flex justify-space-between align-center">
+            <v-card-title class="d-flex ">
                 <div>
                     <h2 class="font-weight-bold">
                         {{ team?.name }}
                     </h2>
-                    <small class="text--secondary">{{ team.branch }}</small>
-                    <small class="text--secondary ml-4">{{ getCategorytext(team.category) }}</small>
-                </div>
-                <div class="d-flex align-center">
                     <v-chip :color="getStatusChipColor(team?.isActive)" class="mr-2" small>
                         {{ getChiptext(team?.isActive) }}
                     </v-chip>
                     <v-chip :color="getStatusChipColor(team?.isPaid)" small>
                         {{ getPaidtext(team?.isPaid) }}
                     </v-chip>
+                    <div>
+                        <small class="text--secondary">{{ team.branch }}</small>
+                        <small class="text--secondary ml-4">{{ getCategorytext(team.category) }}</small>
+                    </div>
                 </div>
             </v-card-title>
-            <v-row v-for="manager in team?.managers" :key="manager.email" dense class="mb-3">
+            <v-row v-for="manager in team?.managers" :key="manager.email" dense>
                 <v-col cols="12">
                     <v-row align="center">
                         <v-col>
@@ -38,39 +38,33 @@
             </v-row>
             <v-divider class="my-3" />
             <!-- Add Player Button -->
-            <v-row justify="center">
-                <v-btn color="primary" class="font-weight-bold mb-2 mt-2" to="/managers/rosters/createrosterplayer">
-                    <v-icon left>
-                        mdi-plus
-                    </v-icon>
-                    AGREGAR JUGADOR/A
-                </v-btn>
-            </v-row>
             <div class="d-flex justify-space-around mt-5">
-                <v-badge color="rgb(var(--v-theme-error))">
-                    <template #badge>
-                        No puede Jugar
-                    </template>
-                </v-badge>
-                <v-badge color="rgb(var(--v-theme-warning))">
-                    <template #badge>
-                        No puede Pitchar
-                    </template>
-                </v-badge>
-                <v-badge color="rgb(var(--v-theme-success))">
-                    <template #badge>
-                        Refuerzo
-                    </template>
-                </v-badge>
+                <v-chip color="rgb(var(--v-theme-error))">
+                    No puede Jugar
+                </v-chip>
+                <v-chip color="rgb(var(--v-theme-warning))">
+                    No puede Pitchar
+                </v-chip>
+                <v-chip color="rgb(var(--v-theme-success))">
+                    Refuerzo
+                </v-chip>
             </div>
             <!-- Players Section -->
-            <v-card class="mt-5">
-                <v-data-iterator :items="team.rosters" :items-per-page="3" :search="search">
+            <v-card class="mt-3">
+                <v-data-iterator :items="team.rosters" :items-per-page="2" :search="search">
                     <template #header>
                         <v-toolbar class="px-2">
-                            <v-text-field v-model="search" density="comfortable" placeholder="Search"
-                                prepend-inner-icon="mdi-magnify" style="max-width: 300px;" variant="solo" clearable
-                                hide-details />
+                            <v-row justify="center">
+                                <v-text-field v-model="search" density="comfortable" placeholder="Search"
+                                    prepend-inner-icon="mdi-magnify" style="max-width: 300px;" variant="solo" clearable
+                                    hide-details />
+                                <v-btn color="primary" class="font-weight-bold mb-2 mt-2"
+                                    to="/managers/rosters/createrosterplayer">
+                                    <v-icon left color="primary">
+                                        mdi-plus
+                                    </v-icon>
+                                </v-btn>
+                            </v-row>
                         </v-toolbar>
                     </template>
 
@@ -90,18 +84,14 @@
                                     </template>
 
                                     <template #title>
-                                        <div class="d-flex justify-space-between ml-5" @click="editPlayer(file.raw.id)">
-                                            <div>
-                                                <h4>{{ file.raw.name }}</h4>
-                                            </div>
-                                            <div class="d-flex mt-2 mr-2">
-                                                <v-badge v-if="file.raw.blockedToPlay"
-                                                    color="rgb(var(--v-theme-error))" />
-                                                <v-badge v-if="file.raw.blockedToPitch"
-                                                    color="rgb(var(--v-theme-warning))" />
-                                                <v-badge v-if="file.raw.isReinforcement"
-                                                    color="rgb(var(--v-theme-success))" class="ml-6" />
-                                            </div>
+                                        <div class=" mb-2" @click="editPlayer(file.raw.id)">
+                                            <h4>{{ file.raw.name }}</h4>
+                                            <v-chip v-if="file.raw.blockedToPlay" color="rgb(var(--v-theme-error))"
+                                                class="mr-2 ml-2" small />
+                                            <v-chip v-if="file.raw.blockedToPitch" color="rgb(var(--v-theme-warning))"
+                                                class="mr-2 ml-2" small />
+                                            <v-chip v-if="file.raw.isReinforcement" color="rgb(var(--v-theme-success))"
+                                                class="mr-2" small />
                                         </div>
                                     </template>
 
